3f175f07ab18a07cb66170a1920da2ab2a49543a
Shahin Motia <chromepanthers@gmail.com>
2017-10-04 01:53:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,14 @@ function logShout(string){
 function logWhisper(string){
   console.log(string.toLowerCase())
 }
+function sayHiToGrandma(string){
+  if (string="I love you, Grandma."){
+  return "I love you, too."
+  }
+  else if (string=string.toLowerCase()){
+    return "I can't hear you!") 
+  }
+  else if (string=string.toUpperCase()) {
+    return "YES INDEED!"
+  }  
+}  
