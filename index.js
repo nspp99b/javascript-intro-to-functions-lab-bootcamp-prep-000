d6bea6c3b911017205f15f2ed1ef25bb6cb6851c
Shahin Motia <chromepanthers@gmail.com>
2017-10-04 02:08:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ function logWhisper(string){
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string){
-  if (string="I love you, Grandma."){
+  if (string==="I love you, Grandma."){
   return "I love you, too."
   }
-  else if (string=string.toLowerCase()){
+  else if (string===string.toLowerCase()){
     return "I can't hear you!")
   }
-  else if (string=string.toUpperCase()) {
+  else if (string===string.toUpperCase()) {
     return "YES INDEED!"
   }
 }
