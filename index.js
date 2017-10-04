bae034809a7aa07e21dad11275df0cc75a5d0adf
Shahin Motia <chromepanthers@gmail.com>
2017-10-04 01:53:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ function sayHiToGrandma(string){
   return "I love you, too."
   }
   else if (string=string.toLowerCase()){
-    return "I can't hear you!") 
+    return "I can't hear you!")
   }
   else if (string=string.toUpperCase()) {
     return "YES INDEED!"
-  }  
-}  
+  }
+}
